e5917e8550c8885c65da9dd0e204e87d8160b500
Brian Anderson <banderson@mozilla.com>
2016-02-28 23:58:21 +0000

    Put the crates on explicit versions

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
-// This library does nothing.
+// This library does nothing but ensure that all the crates
+// can link together.
+
+extern crate bitflags;
+extern crate docopt;
+extern crate env_logger;
+extern crate itertools;
+extern crate lazy_static;
+extern crate libc;
+extern crate log;
+extern crate num;
+extern crate rand;
+extern crate regex;
+extern crate rustc_serialize;
+extern crate semver;
+extern crate tempdir;
+extern crate time;
+extern crate toml;
+extern crate url;
