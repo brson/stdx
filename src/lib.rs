b5e90b91123c39b61a80d28ba992e7485c520db3
Brian Anderson <banderson@mozilla.com>
2017-01-21 19:22:22 -0800

    Reexport crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 // This library does nothing but ensure that all the crates
 // can link together.
 
-extern crate bitflags;
-extern crate docopt;
-extern crate env_logger;
-extern crate itertools;
-extern crate lazy_static;
-extern crate libc;
-extern crate log;
-extern crate num;
-extern crate rand;
-extern crate regex;
-extern crate rustc_serialize;
-extern crate semver;
-extern crate tempdir;
-extern crate time;
-extern crate toml;
-extern crate url;
+pub extern crate bitflags;
+pub extern crate docopt;
+pub extern crate env_logger;
+pub extern crate itertools;
+pub extern crate lazy_static;
+pub extern crate libc;
+pub extern crate log;
+pub extern crate num;
+pub extern crate rand;
+pub extern crate regex;
+pub extern crate rustc_serialize;
+pub extern crate semver;
+pub extern crate tempdir;
+pub extern crate time;
+pub extern crate toml;
+pub extern crate url;
