ac90a77c442d53a7e8cab1b8e7e0ccedd2480c8a
Brian Anderson <banderson@mozilla.com>
2017-01-23 15:30:33 -0800

    Fix quick_sort partition

--- examples/rayon.rs
+++ examples/rayon.rs
@@ -24,7 +24,20 @@ fn quick_sort<T: PartialOrd + Send>(v: &mut [T]) {
         return;
     }
 
-    let mid = v.len() / 2;
+    let mid = partition(v);
     let (lo, hi) = v.split_at_mut(mid);
     rayon::join(|| quick_sort(lo), || quick_sort(hi));
+}
+
+fn partition<T: PartialOrd + Send>(v: &mut [T]) -> usize {
+    let pivot = v.len() - 1;
+    let mut i = 0;
+    for j in 0..pivot {
+        if v[j] <= v[pivot] {
+            v.swap(i, j);
+            i += 1;
+        }
+    }
+    v.swap(i, pivot);
+    i
 }
